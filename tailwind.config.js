7c649d329ce471fdc06e17b7f353f89a4cf50cb1
SnehaSingh <snehasinghbhardwaj@gmail.com>
2023-02-13 18:54:13 +0530

    Assignment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{html,js,ts,jsx,tsx}"];
-export const theme = {
-  extend: {},
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
-export const plugins = [];
